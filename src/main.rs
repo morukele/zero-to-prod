446132b5e09c231cb811746788db345a6a51c91d
Oghenemarho Orukele <orukele.dev@gmail.com>
2023-04-28 20:46:41 +0200

    implemented health_check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello World");
+use std::io;
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
