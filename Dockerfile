a66ff2222fba176782d9821c306292b37f5c4dae
Oghenemarho Orukele <orukele.dev@gmail.com>
2023-05-05 18:08:55 +0200

    added docker build support

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,30 @@
-FROM rust:1.67.0
+# Builder stage
+FROM rust:1.67.0 AS builder
+
 WORKDIR /app
 RUN apt update && apt install lld clang -y
 COPY . .
 ENV SQLX_OFFLINE true
 RUN cargo build --release
-ENTRYPOINT [ "./target/release/zero2prod" ]
+
+# Runtime stage
+FROM debian:bullseye-slim AS runtime
+
+WORKDIR /app
+# Install OpenSSL - it is dynamically linked by some of the dependencies
+# Install ca-certificates needed to verify TLS certificates
+# when establishing HTTPS connections
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends openssl ca-certificates \
+    ## Clean up
+    && apt-get autoremove -y \ 
+    && apt-get clean -y \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy the compiled binary from the builder environment
+# to the runtime environment
+COPY --from=builder /app/target/release/zero2prod zero2prod
+# Copy the configuration file to the runtime environment
+COPY configuration configuration
+ENV APP_ENVIRONMENT production
+ENTRYPOINT [ "./zero2prod" ]
--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let connection_pool =
         PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
-    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
-    // reasons. This binding may cause issues in some environments. For example,
-    // it causes connectivity issues running in WSL2, where you cannot reach the
-    // server when it is bound to WSL2's localhost interface. As a workaround,
-    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
-    // of the security implications when you expose the server on all interfaces.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
